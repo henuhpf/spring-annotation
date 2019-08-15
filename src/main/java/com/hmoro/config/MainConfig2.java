3649b8551958976394146eee624ba71b5393df0c
henuhpf <han024589>
2019-08-15 16:53:46 +0800

    @Conditional 按照条件注册Bean 配置VM-options 时，可以加上 -Dos.name=Windows,来修改程序的运行环境

--- src/main/java/com/hmoro/config/MainConfig2.java
+++ src/main/java/com/hmoro/config/MainConfig2.java
@@ -1,10 +1,15 @@
 package com.hmoro.config;
 
 import com.hmoro.bean.Person;
+import com.hmoro.condition.LinuxCondition;
+import com.hmoro.condition.WindowsCondition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Lazy;
 
+// 满足当前条件，这个类中配置的所有 Bean 才会生效
+//@Conditional({WindowsCondition.class})
 @Configuration
 public class MainConfig2 {
     /*
@@ -17,11 +22,32 @@ public class MainConfig2 {
      * singletion: 单实例 (默认) ,IOC 容器启动会调用方法创建对象放到 IOC 容器中
      * request: 同一次请求创建一个实例
      * session: 同一个 session 创建一个实例
+     *
+     * 懒加载:
+     *      单实例 bean : 默认在容器启动的时候创建对象;
+     *      懒加载 : 容器启动时不创建对象，第一次获取 Bean创建对象，并初始化;
      */
-    @Scope("prototype")
+//    @Scope("prototype")
     @Bean("person")
+    @Lazy
     public Person person(){
         System.out.println("给容器添加person");
         return new Person("张三",25);
+    }
+    /**
+     * @Conditional(condition[]) : 按照一定的条件进行判断，满足条件给容器中注册 bean
+     *
+     * 如果系统是 Windows,给容器注册 tom
+     * 如果系统是 Linux, 给容器注册 jerry
+     */
+    @Conditional({WindowsCondition.class})
+    @Bean("tom")
+    public Person person01(){
+        return new Person("Tom",79);
+    }
+    @Conditional({LinuxCondition.class})
+    @Bean("jerry")
+    public Person person02(){
+        return new Person("Jerry",79);
     }
 }
